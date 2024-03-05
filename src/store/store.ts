ccd2d485e9c8837f207b65ba675958b57552e013
Nyamekesse Samuel <snyamekesse@stu.ucc.edu.gh>
2024-03-05 15:26:50 +0000

    setup anld configured store using Zustand

---
+++ src/store/store.ts
@@ -0,0 +1,19 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import {create} from 'zustand';
+import {createJSONStorage, persist} from 'zustand/middleware';
+import BeansData from '../data/BeansData';
+import CoffeeData from '../data/CoffeeData';
+
+export const useStore = create(
+  persist(
+    (set, get) => ({
+      CoffeeList: CoffeeData,
+      BeanList: BeansData,
+      CartPrice: 0,
+      FavoriteList: [],
+      CartList: [],
+      OrderHistoryList: [],
+    }),
+    {name: 'coffee-app', storage: createJSONStorage(() => AsyncStorage)},
+  ),
+);
