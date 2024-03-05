75473e3d415c800b15f0550f7fdaf0cd5629d57c
Nyamekesse Samuel <snyamekesse@stu.ucc.edu.gh>
2024-03-05 16:44:53 +0000

    created and added components for Header, ProfilePic and GradientBGIcon

---
+++ src/components/ProfilePic.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import {Image, StyleSheet, View} from 'react-native';
+import {COLORS, SPACING} from '../theme/theme';
+
+const ProfilePic = () => {
+  return (
+    <View style={styles.imageContainer}>
+      <Image
+        style={styles.image}
+        source={require('../assets/app_images/avatar.png')}
+      />
+    </View>
+  );
+};
+
+export default ProfilePic;
+
+const styles = StyleSheet.create({
+  imageContainer: {
+    height: SPACING.space_36,
+    width: SPACING.space_36,
+    borderRadius: SPACING.space_12,
+    borderWidth: 2,
+    borderColor: COLORS.secondaryDarkGreyHex,
+    alignItems: 'center',
+    overflow: 'hidden',
+  },
+  image: {
+    height: SPACING.space_36,
+    width: SPACING.space_36,
+  },
+});
