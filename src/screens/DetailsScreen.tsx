dfedb7f81f1f0991e5fbbf7046ef806351e5aab5
Nyamekesse Samuel <snyamekesse@stu.ucc.edu.gh>
2024-03-11 13:49:05 +0000

    added custom created ImageBackgroundInfo component to display selected item image and title details

--- src/screens/DetailsScreen.tsx
+++ src/screens/DetailsScreen.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {ScrollView, StatusBar, StyleSheet, View} from 'react-native';
 
 import {
   NavigationProp,
   ParamListBase,
   RouteProp,
 } from '@react-navigation/native';
+import ImageBackgroundInfo from '../components/ImageBackgroundInfo';
 import {useStore} from '../store/store';
+import {COLORS} from '../theme/theme';
 
 type RootStackParamList = {
   Home: undefined;
@@ -15,22 +17,54 @@ type RootStackParamList = {
 
 type DetailsScreenRouteProp = RouteProp<RootStackParamList, 'Details'>;
 interface Props {
-  navigate: NavigationProp<ParamListBase>;
+  navigation: NavigationProp<ParamListBase>;
   route: DetailsScreenRouteProp;
 }
 
-const DetailsScreen = ({navigate, route}: Props) => {
+const DetailsScreen = ({navigation, route}: Props) => {
+  const addToFavoriteList = useStore((state: any) => state.addToFavoriteList);
+  const deleteFromFavoriteList = useStore(
+    (state: any) => state.deleteFromFavoriteList,
+  );
+  const backHandler = () => {
+    navigation.goBack();
+  };
+  const toggleFavouriteHandler = (
+    favourite: boolean,
+    type: string,
+    id: string,
+  ) => {
+    favourite ? deleteFromFavoriteList(type, id) : addToFavoriteList(type, id);
+  };
   const {type, index, id} = route.params;
   const itemOfIndex = useStore((state: any) =>
     type === 'Coffee' ? state.CoffeeList : state.BeanList,
   )[index];
   return (
-    <View>
-      <Text>DetailsScreen</Text>
+    <View style={styles.screenContainer}>
+      <StatusBar backgroundColor={COLORS.primaryBlackHex} />
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={styles.scrollViewFlex}>
+        <ImageBackgroundInfo
+          enableBackHandler={true}
+          {...itemOfIndex}
+          backHandler={backHandler}
+          toggleFavourite={toggleFavouriteHandler}
+        />
+      </ScrollView>
     </View>
   );
 };
 
 export default DetailsScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  screenContainer: {
+    flex: 1,
+    backgroundColor: COLORS.primaryBlackHex,
+  },
+  scrollViewFlex: {
+    flexGrow: 1,
+  },
+});
