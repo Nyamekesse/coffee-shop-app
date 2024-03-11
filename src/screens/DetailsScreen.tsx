34e1ad9784755f9a449f1f01cc8be096e292b519
Nyamekesse Samuel <snyamekesse@stu.ucc.edu.gh>
2024-03-11 12:05:44 +0000

    retrieved type, index, and id from route params, got itemOfIndex from useStore by checking type

--- src/screens/DetailsScreen.tsx
+++ src/screens/DetailsScreen.tsx
@@ -1,7 +1,29 @@
 import React from 'react';
 import {StyleSheet, Text, View} from 'react-native';
 
-const DetailsScreen = () => {
+import {
+  NavigationProp,
+  ParamListBase,
+  RouteProp,
+} from '@react-navigation/native';
+import {useStore} from '../store/store';
+
+type RootStackParamList = {
+  Home: undefined;
+  Details: {id: string; index: number; type: string};
+};
+
+type DetailsScreenRouteProp = RouteProp<RootStackParamList, 'Details'>;
+interface Props {
+  navigate: NavigationProp<ParamListBase>;
+  route: DetailsScreenRouteProp;
+}
+
+const DetailsScreen = ({navigate, route}: Props) => {
+  const {type, index, id} = route.params;
+  const itemOfIndex = useStore((state: any) =>
+    type === 'Coffee' ? state.CoffeeList : state.BeanList,
+  )[index];
   return (
     <View>
       <Text>DetailsScreen</Text>
