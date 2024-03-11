8cf8332dbf037c3965b24e9dc4606ab90be3fd62
Nyamekesse Samuel <snyamekesse@stu.ucc.edu.gh>
2024-03-11 13:48:01 +0000

    added commit to display selected item details image and details

---
+++ src/components/ImageBackgroundInfo.tsx
@@ -0,0 +1,202 @@
+import React from 'react';
+import {
+  ImageBackground,
+  ImageProps,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+import {
+  BORDERRADIUS,
+  COLORS,
+  FONTFAMILY,
+  FONTSIZE,
+  SPACING,
+} from '../theme/theme';
+import CustomIcon from './CustomIcon';
+import GradientBGIcon from './GradientBGIcon';
+
+interface ImageBackgroundInfoProps {
+  enableBackHandler: boolean;
+  imagelink_portrait: ImageProps;
+  type: string;
+  id: string;
+  favourite: boolean;
+  name: string;
+  special_ingredient: string;
+  ingredients: string;
+  average_rating: number;
+  ratings_count: string;
+  roasted: string;
+  backHandler: () => void;
+  toggleFavourite: (favourite: boolean, type: string, id: string) => void;
+}
+
+const ImageBackgroundInfo: React.FC<ImageBackgroundInfoProps> = ({
+  average_rating,
+  enableBackHandler,
+  favourite,
+  id,
+  imagelink_portrait,
+  ingredients,
+  name,
+  ratings_count,
+  roasted,
+  special_ingredient,
+  toggleFavourite,
+  type,
+  backHandler,
+}) => {
+  return (
+    <View>
+      <ImageBackground
+        style={styles.itemBackgroundImage}
+        source={imagelink_portrait}>
+        {enableBackHandler ? (
+          <View style={styles.imageHeaderBarContainerWithBack}>
+            <TouchableOpacity
+              onPress={() => {
+                backHandler();
+              }}>
+              <GradientBGIcon
+                name="left"
+                color={COLORS.primaryLightGreyHex}
+                size={FONTSIZE.size_16}
+              />
+            </TouchableOpacity>
+            <TouchableOpacity
+              onPress={() => toggleFavourite(favourite, type, id)}>
+              <GradientBGIcon
+                name="like"
+                color={
+                  favourite ? COLORS.primaryRedHex : COLORS.primaryLightGreyHex
+                }
+                size={FONTSIZE.size_16}
+              />
+            </TouchableOpacity>
+          </View>
+        ) : (
+          <View style={styles.imageHeaderBarContainerWithoutBack}>
+            <TouchableOpacity>
+              <GradientBGIcon
+                name="like"
+                color={
+                  favourite ? COLORS.primaryRedHex : COLORS.primaryLightGreyHex
+                }
+                size={FONTSIZE.size_16}
+              />
+            </TouchableOpacity>
+          </View>
+        )}
+        <View style={styles.imageInfoOuterContainer}>
+          <View style={styles.imageInfoInnerContainer}>
+            <View style={styles.infoContainerRow}>
+              <View>
+                <Text style={styles.itemTitleText}>{name}</Text>
+                <Text style={styles.itemSubtitleText}>
+                  {special_ingredient}
+                </Text>
+              </View>
+              <View style={styles.itemPropertiesContainer}>
+                <View style={styles.firstProperty}>
+                  <CustomIcon
+                    name={type === 'Bean' ? 'bean' : 'beans'}
+                    size={type === 'Bean' ? FONTSIZE.size_18 : FONTSIZE.size_24}
+                    color={COLORS.primaryOrangeHex}
+                  />
+                  <Text
+                    style={[
+                      styles.firstTextProperty,
+                      {
+                        marginTop:
+                          type === 'Bean'
+                            ? SPACING.space_4 + SPACING.space_2
+                            : 0,
+                      },
+                    ]}>
+                    {type}
+                  </Text>
+                </View>
+                <View style={styles.firstProperty}>
+                  <CustomIcon
+                    name={type === 'Bean' ? 'location' : 'drop'}
+                    size={FONTSIZE.size_16}
+                    color={COLORS.primaryOrangeHex}
+                  />
+                  <Text style={styles.firstTextProperty}>{ingredients}</Text>
+                </View>
+              </View>
+            </View>
+          </View>
+        </View>
+      </ImageBackground>
+    </View>
+  );
+};
+
+export default ImageBackgroundInfo;
+
+const styles = StyleSheet.create({
+  itemBackgroundImage: {
+    width: '100%',
+    aspectRatio: 20 / 25,
+    justifyContent: 'space-between',
+  },
+  imageHeaderBarContainerWithBack: {
+    padding: SPACING.space_30,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  imageHeaderBarContainerWithoutBack: {
+    padding: SPACING.space_30,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+  },
+  imageInfoOuterContainer: {
+    paddingVertical: SPACING.space_24,
+    paddingHorizontal: SPACING.space_30,
+    backgroundColor: COLORS.primaryBlackRGBA,
+    borderTopLeftRadius: BORDERRADIUS.radius_20 * 2,
+    borderTopRightRadius: BORDERRADIUS.radius_20 * 2,
+  },
+  imageInfoInnerContainer: {
+    justifyContent: 'space-between',
+    gap: SPACING.space_15,
+  },
+  infoContainerRow: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  itemTitleText: {
+    fontFamily: FONTFAMILY.poppins_semibold,
+    fontSize: FONTSIZE.size_24,
+    color: COLORS.primaryWhiteHex,
+  },
+  itemSubtitleText: {
+    fontFamily: FONTFAMILY.poppins_medium,
+    fontSize: FONTSIZE.size_12,
+    color: COLORS.primaryWhiteHex,
+  },
+  itemPropertiesContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: SPACING.space_20,
+  },
+  firstProperty: {
+    height: 55,
+    width: 55,
+    borderRadius: BORDERRADIUS.radius_15,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: COLORS.primaryBlackHex,
+  },
+  firstTextProperty: {
+    fontFamily: FONTFAMILY.poppins_medium,
+    fontSize: FONTSIZE.size_10,
+    color: COLORS.primaryWhiteHex,
+  },
+});
