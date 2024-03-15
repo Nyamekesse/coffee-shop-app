15f19c12a9cfce2b0fa7ac233fc4c62f6cef51b1
Nyamekesse Samuel <snyamekesse@stu.ucc.edu.gh>
2024-03-15 11:20:33 +0000

    defined payment list, added payment method component to render payment methods types

--- src/screens/PaymentScreen.tsx
+++ src/screens/PaymentScreen.tsx
@@ -1,14 +1,103 @@
-import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import React, {useState} from 'react';
+import {
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+import GradientBGIcon from '../components/GradientBGIcon';
+import PaymentMethod from '../components/PaymentMethod';
+import {COLORS, FONTFAMILY, FONTSIZE, SPACING} from '../theme/theme';
+
+const paymentList = [
+  {
+    name: 'Wallet',
+    icon: 'icon',
+    isIcon: true,
+  },
+  {
+    name: 'Google Pay',
+    icon: require('../assets/app_images/gpay.png'),
+    isIcon: false,
+  },
+  {
+    name: 'Apple Pay',
+    icon: require('../assets/app_images/applepay.png'),
+    isIcon: false,
+  },
+  {
+    name: 'Amazon Pay',
+    icon: require('../assets/app_images/amazonpay.png'),
+    isIcon: false,
+  },
+];
 
 const PaymentScreen = () => {
+  const [paymentMode, setPaymentMode] = useState('Credit Card');
   return (
-    <View>
-      <Text>PaymentScreen</Text>
+    <View style={styles.screenContainer}>
+      <StatusBar backgroundColor={COLORS.primaryBlackHex} />
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={styles.scrollViewFlex}>
+        <View style={styles.headerContainer}>
+          <TouchableOpacity>
+            <GradientBGIcon
+              name="left"
+              color={COLORS.primaryWhiteHex}
+              size={FONTSIZE.size_16}
+            />
+          </TouchableOpacity>
+          <Text style={styles.headerText}>Payments</Text>
+          <View style={styles.emptyView}></View>
+        </View>
+        <View style={styles.paymentOptionsContainer}>
+          {paymentList.map((paymentType: any, index: number) => (
+            <TouchableOpacity key={index}>
+              <PaymentMethod
+                paymentMode={paymentMode}
+                name={paymentType.name}
+                icon={paymentType.icon}
+                isIcon={paymentType.isIcon}
+              />
+            </TouchableOpacity>
+          ))}
+        </View>
+      </ScrollView>
     </View>
   );
 };
 
 export default PaymentScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  screenContainer: {
+    flex: 1,
+    backgroundColor: COLORS.primaryBlackHex,
+  },
+  scrollViewFlex: {
+    flexGrow: 1,
+  },
+  headerContainer: {
+    paddingHorizontal: SPACING.space_24,
+    paddingVertical: SPACING.space_15,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  headerText: {
+    fontFamily: FONTFAMILY.poppins_semibold,
+    fontSize: FONTSIZE.size_20,
+    color: COLORS.primaryWhiteHex,
+  },
+  emptyView: {
+    height: SPACING.space_36,
+    width: SPACING.space_36,
+  },
+  paymentOptionsContainer: {
+    padding: SPACING.space_15,
+    gap: SPACING.space_15,
+  },
+});
